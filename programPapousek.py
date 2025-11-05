9308d0d7aa65fd01e313a2b360803ca48a297935
jophymba <jophy.mba@icloud.com>
2025-11-05 17:35:53 +0700

    second commit

--- programPapousek.py
+++ programPapousek.py
@@ -15,3 +15,18 @@ print("Zadejte cislo k nasobeni")
 b = input()
 b = b * 2
 print(b)
+
+print ("Vitejte v kalkulacce")
+print("Zadejte prvni cislo: ")
+a = float(input())
+print("Zadejte druhe cislo: ")
+b = float(input())
+soucet = a + b
+rozdil = a - b
+soucin = a * b
+podil = a / b
+print(f"soucet: {soucet}")
+print(f"rozdil: {rozdil}")
+print(f"soucin: {soucin}")
+print(f"podil: {podil}")
+print("Dekuji za pouziti kalkulacky")
