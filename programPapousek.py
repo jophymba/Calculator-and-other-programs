355dc08699655620decba64f5ce9a698e15159fb
jophymba <jophy.mba@icloud.com>
2025-11-05 17:05:01 +0700

    firstCommit

---
+++ programPapousek.py
@@ -0,0 +1,17 @@
+print("Ahoj jsem program papousek a vse opakuji")
+print("Napis neco")
+
+vstup = input()
+vystup = vstup + " ,  " + vstup + "!"
+print(vystup)
+
+
+print("Zadejte cislo k nasobeni")
+a = int(input())
+a = a * 2
+print(a)
+
+print("Zadejte cislo k nasobeni")
+b = input()
+b = b * 2
+print(b)
